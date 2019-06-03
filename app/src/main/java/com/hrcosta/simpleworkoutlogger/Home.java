eea379c443c8d26c2b9a0006a6a16093e08a501f
Helton <hrcosta@scopus.com.br>
2019-06-03 19:06:47 -0300

    colors updated

--- app/src/main/java/com/hrcosta/simpleworkoutlogger/Home.java
+++ app/src/main/java/com/hrcosta/simpleworkoutlogger/Home.java
@@ -2,11 +2,17 @@ package com.hrcosta.simpleworkoutlogger;
 
 import android.app.Application;
 import android.content.Intent;
+import android.util.Log;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import static android.content.ContentValues.TAG;
+
 public class Home extends Application {
+
+    private static final String TAG = Home.class.getSimpleName();
+
 
     @Override
     public void onCreate() {
@@ -16,6 +22,7 @@ public class Home extends Application {
         FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
 
         if (firebaseUser != null){
+            Log.d(TAG, "onCreate: Home class calling the Calendar Activity..");
             startActivity(new Intent(Home.this, CalendarActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
         }
     }
